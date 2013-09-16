fc7ad53d3f5e9fb84ee10372c0538ba455cd9867
mfullen <mike@mikefullen.com>
2013-09-15 20:39:03 -0400

    hw2 unit tests updated for TDD

---
+++ part1/homework2/src/hw2/Hw2App.py
@@ -0,0 +1,10 @@
+class Hw2App(object):
+    
+    
+    def calc_event_study(self, startDate, endDate, eventPrice):
+        return 0
+    
+    
+
+
+
--- part1/homework2/src/hw2/test/Hw2Test.py
+++ part1/homework2/src/hw2/test/Hw2Test.py
@@ -8,26 +8,29 @@ Computational Investing Part 1 HW 2: Event Studies
 import unittest
 
 import datetime as dt
+from hw2.Hw2App import Hw2App
 
 class Hw2Test(unittest.TestCase):
 
 
     def testExample1(self):
         print("======================testExample1===============")
+        app = Hw2App()
         startDate = dt.datetime(2008,1,1)
         endDate = dt.datetime(2009,1,1)
         eventPrice = 5.0
         expectedEvents = 176
-        resultEventNumber = 0
+        resultEventNumber = app.calc_event_study(startDate,endDate,eventPrice)
         self.assertEqual(expectedEvents, resultEventNumber, "Events are not equal")
 
     def testExample2(self):
         print("======================testExample2===============")
+        app = Hw2App()
         startDate = dt.datetime(2008,1,1)
         endDate = dt.datetime(2009,1,1)
         eventPrice = 5.0
         expectedEvents = 326
-        resultEventNumber = 0
+        resultEventNumber = app.calc_event_study(startDate,endDate,eventPrice)
         self.assertEqual(expectedEvents, resultEventNumber, "Events are not equal")
 
 
