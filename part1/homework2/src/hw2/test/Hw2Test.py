1813ccc0cf9b4c709766037cc31534121f1fb20a
mfullen <mike@mikefullen.com>
2013-09-18 19:25:58 -0400

    Revert "hw2 unit tests updated for TDD"
    This reverts commit fc7ad53d3f5e9fb84ee10372c0538ba455cd9867.

--- part1/homework2/src/hw2/test/Hw2Test.py
+++ part1/homework2/src/hw2/test/Hw2Test.py
@@ -8,29 +8,26 @@ Computational Investing Part 1 HW 2: Event Studies
 import unittest
 
 import datetime as dt
-from hw2.Hw2App import Hw2App
 
 class Hw2Test(unittest.TestCase):
 
 
     def testExample1(self):
         print("======================testExample1===============")
-        app = Hw2App()
         startDate = dt.datetime(2008,1,1)
         endDate = dt.datetime(2009,1,1)
         eventPrice = 5.0
         expectedEvents = 176
-        resultEventNumber = app.calc_event_study(startDate,endDate,eventPrice)
+        resultEventNumber = 0
         self.assertEqual(expectedEvents, resultEventNumber, "Events are not equal")
 
     def testExample2(self):
         print("======================testExample2===============")
-        app = Hw2App()
         startDate = dt.datetime(2008,1,1)
         endDate = dt.datetime(2009,1,1)
         eventPrice = 5.0
         expectedEvents = 326
-        resultEventNumber = app.calc_event_study(startDate,endDate,eventPrice)
+        resultEventNumber = 0
         self.assertEqual(expectedEvents, resultEventNumber, "Events are not equal")
 
 
