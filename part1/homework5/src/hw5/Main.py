f18b0af35a2e9988d7f7b71e23284df70bda299b
mfullen <mike@mikefullen.com>
2013-10-22 21:03:30 -0400

    Week 5 hw updated for graph

--- part1/homework5/src/hw5/Main.py
+++ part1/homework5/src/hw5/Main.py
@@ -12,14 +12,14 @@ import QSTK.qstkutil.qsdateutil as du
 import QSTK.qstkutil.DataAccess as da
 import QSTK.qstkutil.tsutil as tsu
 import QSTK.qstkstudy.EventProfiler as ep
-
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
     print "Hello World"
     ls_symbols = ['AAPL','GOOG','IBM','MSFT']
     dt_start = dt.datetime(2010,1,1 )
-    dt_end = dt.datetime(2010, 12, 31)
+    dt_end = dt.datetime(2010, 6, 15)
     dt_timeofday = dt.timedelta(hours=16)
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
     c_dataobj = da.DataAccess('Yahoo')
@@ -37,3 +37,22 @@ if __name__ == '__main__':
     bollinger_val = (prices - rolling_mean)/rolling_std
     
     print bollinger_val.values
+    
+    plt.clf()
+    plt.subplot(211)
+    plt.plot(ldt_timestamps, prices['GOOG'], label='Google')
+    plt.legend()
+    plt.ylabel('Price')
+    plt.xlabel('Date')
+    plt.xticks(size='xx-small')
+    plt.xlim(ldt_timestamps[0], ldt_timestamps[-1])
+    plt.subplot(212)
+    plt.plot(ldt_timestamps, bollinger_val['GOOG'], label='Google-Bollinger')
+    plt.axhline(1.0, color='r')
+    plt.axhline(-1.0, color='r')
+    plt.legend()
+    plt.ylabel('Bollinger')
+    plt.xlabel('Date')
+    plt.xticks(size='xx-small')
+    plt.xlim(ldt_timestamps[0], ldt_timestamps[-1])
+    plt.savefig('homework5.pdf', format='pdf')
